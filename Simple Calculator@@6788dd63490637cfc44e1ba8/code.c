e2a4dc1642c83ba0401105bfcde6f5308f10f70f
tanishkagoyal164 <tanishka1829.becse24@chitkara.edu.in>
2025-01-28 17:11:41 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -13,7 +13,10 @@ int main() {
     else if(c=='*'){
         printf("%d",a*b);
     }
-    else if(c=='/'){printf("%d",a/b);}
+    else if(c=='/'){
+        if(b!=0){printf("%d",a/b);}
+        else{printf("error");}
+    }
     else{printf("error");}
     return 0;
 }
