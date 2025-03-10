4e357024e5d5effef82558b837300377691f51da
tanishkagoyal164 <tanishka1829.becse24@chitkara.edu.in>
2025-03-10 14:49:57 +0530

    Solution to Find the Maximum and Minimum Element in an Array

--- Find the Maximum and Minimum Element in an Array@@676c02002dbc8c3e7a280b70/code.c
+++ Find the Maximum and Minimum Element in an Array@@676c02002dbc8c3e7a280b70/code.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
-#include <limits.h>
 
-int main() {
-    int n,numbers[1000],i,max=maximum,min=minimum;
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-        scanf("%d",&numbers[i]);
-        if(numbers[i]>max){
-            max=numbers[i];
-        }
-        else{
-            max=max;
-        }
-        if(numbers[i]<min){
-            min=numbers[i];
-        }
-        else{
-            min=min;
+int findFirstPeak(int arr[], int n) {
+    if (n <= 0) {
+        return -1; // Invalid input size
+    }
+
+    if (n == 1) {
+        return arr[0];
+    }
+
+    if (arr[0] > arr[1]) {
+        return arr[0];
+    }
+
+    if (arr[n - 1] > arr[n - 2]) {
+        return arr[n - 1];
+    }
+
+    for (int i = 1; i < n - 1; i++) {
+        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+            return arr[i];
         }
     }
-    printf("%d %d",max,min);
+
+    return -1; // No peak found
+}
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        printf("Invalid input\n");
+        return 1;
+    }
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        if (scanf("%d", &arr[i]) != 1) {
+            printf("Invalid input\n");
+            return 1;
+        }
+    }
+
+    int peak = findFirstPeak(arr, n);
+    printf("%d\n", peak);
+
     return 0;
 }
