c9227f3e37bf866df757336e3a7ae4c906fe4ccd
tanishkagoyal164 <tanishka1829.becse24@chitkara.edu.in>
2025-04-10 14:52:24 +0530

    Solution to Check if a String is a Palindrome

---
+++ Check if a String is a Palindrome@@676c039d80e1714600f67642/code.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100],strrev[100];
+    scanf("%s",&str);
+    int len=strlen(str);
+    for(int i=len-1;i<=0;i--){
+        strrev[len-i-1]=str[i];
+    }
+    if(strrev==str){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+}
